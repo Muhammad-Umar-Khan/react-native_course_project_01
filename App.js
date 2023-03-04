39c4b5e66bf34c448d4ae6455717d43e7aff6758
Muhammad-Umar-Khan <umarasaboor@gmail.com>
2023-03-04 14:58:51 +0500

    file structure and commom components

--- App.js
+++ App.js
@@ -1,11 +1,113 @@
 import React from 'react';
-import {Text, View} from 'react-native';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
+
 const App = () => {
   return (
-    <View>
-      <Text>Hello World</Text>
-    </View>
+    <SafeAreaView>
+      <StatusBar barStyle={'dark-content'} />
+      <ScrollView contentContainerStyle={styles.container}>
+        <View>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+          <Text>Hello World</Text>
+        </View>
+      </ScrollView>
+    </SafeAreaView>
   );
 };
 
 export default App;
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 20,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: '#F5F5F5',
+    color: '#000000',
+  },
+});
